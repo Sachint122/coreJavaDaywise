454a7279ccee966cd2540f616087d0ab87c0c54f
sachi <sachi@jarvishightech>
2024-10-09 13:06:47 +0530

    my fist code

---
+++ src/main/java/com/test/p1/TestProgramm.java
@@ -0,0 +1,10 @@
+package com.test.p1;
+
+public class TestProgramm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("hii i m from eclipse");
+	}
+
+}
